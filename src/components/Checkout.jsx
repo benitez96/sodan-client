e3319ad0f32d137ec659d8872ab6077dc7ccc725
Dan <danii-benitez@hotmail.com>
2023-01-08 22:40:22 -0300

    [add] shipping form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux"
 import CartItem from "./CartItem"
 
 import logo from '../assets/logo.jpeg'
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useEffect } from "react"
+import { Field, Formik } from "formik"
+import * as Yup from 'yup';
 
 export const Checkout = () => {
 
@@ -17,6 +19,40 @@ export const Checkout = () => {
 		}
 	}, [cart])
 
+
+	const shippingForm = {
+		cart,
+		name: '',
+		lastname: '',
+		dni: '',
+		phone: '',
+		email: '',
+		street: '',
+		street_number: '',
+		neighborhood: '',
+		city: '',
+		state: '',
+		zip_code: '',
+	}
+
+	const validateSchema = Yup.object().shape({
+		name: Yup.string().required('Este campo es requerido'),
+		lastname: Yup.string().required('Este campo es requerido'),
+		dni: Yup.string().required('Este campo es requerido'),
+		phone: Yup.string().required('Este campo es requerido'),
+		email: Yup.string().email('Correo invalido').required('Este campo es requerido'),
+		street: Yup.string().required('Este campo es requerido'),
+		city: Yup.string().required('Este campo es requerido'),
+		// state: Yup.string().required('Este campo es requerido'),
+		zip_code: Yup.string().required('Este campo es requerido'),
+
+	})
+
+	const handleSubmit = (values) => {
+		console.log(values)
+	}
+
+
 	return (
 		<section>
 			<h1 className="sr-only">Checkout</h1>
@@ -24,9 +60,9 @@ export const Checkout = () => {
 			<div className="grid grid-cols-1 mx-auto max-w-screen-2xl md:grid-cols-2">
 				<div className="py-12 bg-gray-50 md:py-24">
 					<div className="max-w-lg px-4 mx-auto space-y-8 lg:px-8">
-						<div className="flex items-center">
-							<img src={logo} className='w-20' />
-						</div>
+						<Link to='/' className="flex items-center justify-center">
+							<img src={logo} className='h-20' />
+						</Link>
 
 						<div>
 							<p className="text-2xl font-medium tracking-tight text-gray-900">
@@ -40,7 +76,7 @@ export const Checkout = () => {
 							<div className="flow-root">
 								<ul className="-my-4 divide-y divide-gray-100">
 									{
-										cart.items.map(item => <CartItem product={item} />)
+										cart.items.map(item => <CartItem product={item} key={item.internal_id} />)
 									}
 								</ul>
 							</div>
@@ -48,149 +84,212 @@ export const Checkout = () => {
 					</div>
 				</div>
 
-				<div className="py-12 bg-white md:py-24">
+				<div className="py-6 bg-white md:py-12">
 					<div className="max-w-lg px-4 mx-auto lg:px-8">
-						<form className="grid grid-cols-6 gap-4">
-							<div className="col-span-3">
-								<label
-									for="FirstName"
-									className="block text-xs font-medium text-gray-700"
-								>
-									Nombre
-								</label>
-
-								<input
-									type="text"
-									id="FirstName"
-									className="w-full mt-1 border-gray-200 rounded-md shadow-sm sm:text-sm"
-								/>
-							</div>
-
-							<div className="col-span-3">
-								<label
-									for="LastName"
-									className="block text-xs font-medium text-gray-700"
-								>
-									Last Name
-								</label>
-
-								<input
-									type="text"
-									id="LastName"
-									className="w-full mt-1 border-gray-200 rounded-md shadow-sm sm:text-sm"
-								/>
-							</div>
-
-							<div className="col-span-6">
-								<label for="Email" className="block text-xs font-medium text-gray-700">
-									Email
-								</label>
-
-								<input
-									type="email"
-									id="Email"
-									className="w-full mt-1 border-gray-200 rounded-md shadow-sm sm:text-sm"
-								/>
-							</div>
-
-							<div className="col-span-6">
-								<label for="Phone" className="block text-xs font-medium text-gray-700">
-									Phone
-								</label>
-
-								<input
-									type="tel"
-									id="Phone"
-									className="w-full mt-1 border-gray-200 rounded-md shadow-sm sm:text-sm"
-								/>
-							</div>
-
-							<fieldset className="col-span-6">
-								<legend className="block text-sm font-medium text-gray-700">
-									Card Details
-								</legend>
-
-								<div className="mt-1 -space-y-px bg-white rounded-md shadow-sm">
-									<div>
-										<label for="CardNumber" className="sr-only"> Card Number </label>
-
-										<input
-											type="text"
-											id="CardNumber"
-											placeholder="Card Number"
-											className="relative w-full mt-1 border-gray-200 rounded-t-md focus:z-10 sm:text-sm"
-										/>
-									</div>
-
-									<div className="flex -space-x-px">
-										<div className="flex-1">
-											<label for="CardExpiry" className="sr-only"> Card Expiry </label>
-
-											<input
-												type="text"
-												id="CardExpiry"
-												placeholder="Expiry Date"
-												className="relative w-full border-gray-200 rounded-bl-md focus:z-10 sm:text-sm"
+						<Formik
+							initialValues={shippingForm}
+							validationSchema={validateSchema}
+							onSubmit={handleSubmit}
+						>
+							{
+								props =>
+									<form className="grid grid-cols-6 gap-2" onSubmit={props.handleSubmit}>
+										<legend className="col-span-6">Datos de envio</legend>
+										<div className="col-span-3">
+											<label
+												htmlFor="name"
+												className="text-xs font-medium text-gray-700"
+											>
+												Nombre
+											</label>
+											<Field
+												name="name"
+												className={`${props.errors.name && props.touched.name ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
 											/>
+											{
+												props.errors.name && props.touched.name
+												&&
+												<div className="text-xs text-red-700">{props.errors.name}</div>
+											}
+
 										</div>
 
-										<div className="flex-1">
-											<label for="CardCVC" className="sr-only"> Card CVC </label>
+										<div className="col-span-3">
+											<label
+												htmlFor="lastname"
+												className="text-xs font-medium text-gray-700"
+											>
+												Apellido
+											</label>
 
-											<input
-												type="text"
-												id="CardCVC"
-												placeholder="CVC"
-												className="relative w-full border-gray-200 rounded-br-md focus:z-10 sm:text-sm"
+											<Field
+												name="lastname"
+												className={`${props.errors.lastname && props.touched.lastname ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
 											/>
+											{
+												props.errors.lastname && props.touched.lastname
+												&&
+												<div className="text-xs text-red-700">{props.errors.lastname}</div>
+											}
 										</div>
-									</div>
-								</div>
-							</fieldset>
 
-							<fieldset className="col-span-6">
-								<legend className="block text-sm font-medium text-gray-700">
-									Billing Address
-								</legend>
+										<div className="col-span-6">
+											<label
+												htmlFor="dni"
+												className="text-xs font-medium text-gray-700"
+											>
+												DNI o CUIL
+											</label>
 
-								<div className="mt-1 -space-y-px bg-white rounded-md shadow-sm">
-									<div>
-										<label for="Country" className="sr-only">Country</label>
+											<Field
+												name="dni"
+												className={`${props.errors.dni && props.touched.dni ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.dni && props.touched.dni
+												&&
+												<div className="text-xs text-red-700">{props.errors.dni}</div>
+											}
+										</div>
 
-										<select
-											id="Country"
-											className="relative w-full border-gray-200 rounded-t-md focus:z-10 sm:text-sm"
-										>
-											<option>England</option>
-											<option>Wales</option>
-											<option>Scotland</option>
-											<option>France</option>
-											<option>Belgium</option>
-											<option>Japan</option>
-										</select>
-									</div>
+										<div className="col-span-6">
+											<label htmlFor="email" className="block text-xs font-medium text-gray-700">
+												Email
+											</label>
 
-									<div>
-										<label className="sr-only" for="PostalCode"> ZIP/Post Code </label>
+											<Field
+												name="email"
+												className={`${props.errors.email && props.touched.email ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.email && props.touched.email
+												&&
+												<div className="text-xs text-red-700">{props.errors.email}</div>
+											}
+										</div>
 
-										<input
-											type="text"
-											id="PostalCode"
-											placeholder="ZIP/Post Code"
-											className="relative w-full border-gray-200 rounded-b-md focus:z-10 sm:text-sm"
-										/>
-									</div>
-								</div>
-							</fieldset>
+										<div className="col-span-6">
+											<label htmlFor="phone" className="block text-xs font-medium text-gray-700">
+												Telefono
+											</label>
 
-							<div className="col-span-6">
-								<button
-									className="block w-full rounded-md bg-black p-2.5 text-sm text-white transition hover:shadow-lg"
-								>
-									Pay Now
-								</button>
-							</div>
-						</form>
+											<Field
+												name="phone"
+												className={`${props.errors.phone && props.touched.phone ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.phone && props.touched.phone
+												&&
+												<div className="text-xs text-red-700">{props.errors.phone}</div>
+											}
+										</div>
+										<div className="col-span-3">
+											<label htmlFor="street" className="block text-xs font-medium text-gray-700">
+												Calle
+											</label>
+
+											<Field
+												name="street"
+												className={`${props.errors.street && props.touched.street ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.street && props.touched.street
+												&&
+												<div className="text-xs text-red-700">{props.errors.street}</div>
+											}
+										</div>
+										<div className="col-span-3">
+											<label htmlFor="street_number" className="block text-xs font-medium text-gray-700">
+												Numero (opcional)
+											</label>
+
+											<Field
+												name="street_number"
+												className={`${props.errors.street_number && props.touched.street_number ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.street_number && props.touched.street_number
+												&&
+												<div className="text-xs text-red-700">{props.errors.street_number}</div>
+											}
+										</div>
+										<div className="col-span-3">
+											<label htmlFor="neighborhood" className="block text-xs font-medium text-gray-700">
+												Barrio (opcional)
+											</label>
+
+											<Field
+												name="neighborhood"
+												className={`${props.errors.neighborhood && props.touched.neighborhood ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.neighborhood && props.touched.neighborhood
+												&&
+												<div className="text-xs text-red-700">{props.errors.neighborhood}</div>
+											}
+										</div>
+
+										<div className="col-span-3">
+
+											<label htmlFor="city" className="block text-xs font-medium text-gray-700">
+												Ciudad
+											</label>
+
+											<Field
+												name="city"
+												className={`${props.errors.city && props.touched.city ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.city && props.touched.city
+												&&
+												<div className="text-xs text-red-700">{props.errors.city}</div>
+											}
+										</div>
+
+										<div className="col-span-3">
+											<label htmlFor="zip_code" className="block text-xs font-medium text-gray-700">
+												Codigo Postal
+											</label>
+
+											<Field
+												name="zip_code"
+												className={`${props.errors.zip_code && props.touched.zip_code ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.zip_code && props.touched.zip_code
+												&&
+												<div className="text-xs text-red-700">{props.errors.zip_code}</div>
+											}
+										</div>
+
+										<div className="col-span-3">
+											<label htmlFor="state" className="block text-xs font-medium text-gray-700">
+												Provincia
+											</label>
+
+											<Field
+												name="state"
+												className={`${props.errors.state && props.touched.state ? 'border-red-700' : 'border-gray-200'} w-full mt-1 rounded-md shadow-sm sm:text-sm p-2 border`}
+											/>
+											{
+												props.errors.state && props.touched.state
+												&&
+												<div className="text-xs text-red-700">{props.errors.state}</div>
+											}
+										</div>
+
+										<div className="col-span-6">
+											<button
+												className="block w-full rounded-md bg-black p-2.5 text-sm text-white transition hover:shadow-lg"
+												type="submit"
+											>
+												Pagar
+											</button>
+										</div>
+
+									</form>
+							}
+						</Formik>
 					</div>
 				</div>
 			</div>

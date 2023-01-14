7c32d67f28cb3d7f66d5ff75c89c301419642fce
Dan <danii-benitez@hotmail.com>
2023-01-14 10:54:49 -0300

    [add] link to ig on footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Cart } from "./Cart"
 import { Header } from "./Header"
+import { AiOutlineInstagram } from 'react-icons/ai'
 
 
 export const Layout = ({ children }) => {
@@ -10,7 +11,10 @@ export const Layout = ({ children }) => {
 			<div className="container mx-auto">
 				{children}
 			</div>
-			<footer className="text-gray-500 text-center p-3 text-xs">SODAN 2023</footer>
+			<footer className="text-gray-500 flex flex-col items-center justify-center gap-2 p-3 text-xs">
+				<a href="https://www.instagram.com/SODAN_CLOTHES" target='_blank' className='text-2xl flex items-center gap-1'><AiOutlineInstagram /><div className="text-lg">SODAN_CLOTHES</div></a>
+				<span>SODAN 2023 ©</span>
+			</footer>
 		</main>
 	)
 }

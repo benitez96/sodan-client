cc20752f29724be8f56e7240f63c602a7ec47751
Dan <danii-benitez@hotmail.com>
2023-01-20 18:33:14 -0300

    [add] format json to products get

--- src/actions/products.js
+++ src/actions/products.js
@@ -5,7 +5,7 @@ import { product_types } from "../types/types"
 export const load_products = () => {
 
 	return async (dispatch) => {
-		const products = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/products/`).then(res => res.data)
+		const products = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/products/?format=json`).then(res => res.data)
 		dispatch({ type: product_types.load, payload: products })
 
 	}
@@ -15,7 +15,7 @@ export const load_products = () => {
 export const get_product_by_slug = (slug) => {
 
 	return async (dispatch) => {
-		const product = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/products/${slug}`).then(res => res.data).catch(res => 'not found')
+		const product = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/products/${slug}/?format=json`).then(res => res.data).catch(res => 'not found')
 		console.log(product)
 		dispatch({ type: product_types.load_active, payload: product })
 

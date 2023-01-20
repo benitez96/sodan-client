a5fff323f73524747b2e0022249e88cfdeace9b6
Dan <danii-benitez@hotmail.com>
2023-01-20 19:42:03 -0300

    [fix] logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { CartButton } from "./CartButton"
-import logo from '../assets/logo.jpeg'
+import logo from '../assets/sodan.svg'
 import { Announcement } from "./Announcement"
 import { useEffect, useState } from 'react';
 
